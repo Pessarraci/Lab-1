4a53fff3663946fbddebdfa485350db8d9574339
Pessarraci <78044004+Pessarraci@users.noreply.github.com>
2021-01-26 13:44:11 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,23 @@
+/*********************
+*Lab1
+*********************
+*Carl Pessarra	
+*01/26/21
+*CMSC-255-003
+**********************/
+
+public class Hello{
+	
+	public static void main(String[] args) {
+		
+	 System.out.println("<Carl Pessarra>");
+     System.out.println("<Hello>");
+     System.out.println("<For Lab 1>");
+     System.out.println("<Version Date>");
+     System.out.println("<CMSC-255-003>");
+     System.out.println();
+	}
+}
+
+
+
